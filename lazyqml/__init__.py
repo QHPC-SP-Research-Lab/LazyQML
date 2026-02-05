7d90fd2f54cbf795d080530ba3c8eae98644fe10
Jose Ranilla Pastor <ranilla@pirserver.edv.uniovi.es>
2026-02-05 21:06:27 +0100

    Bump version: 0.0.17 → 0.0.18

--- lazyqml/__init__.py
+++ lazyqml/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """Diego García Vega, Fernando Álvaro Plou Llorente, Alejandro Leal Castaño"""
 __email__ = "garciavdiego@uniovi.es, ploufernando@uniovi.es, lealcalejandro@uniovi.es"
-__version__ = "0.0.17"
+__version__ = "0.0.18"
 
 from .lazyqml import QuantumClassifier
 
