7db8474f41ad66ff04f2b52d89621de48de230a5
Jose Ranilla Pastor <ranilla@pirserver.edv.uniovi.es>
2026-04-17 17:57:01 +0200

    Bump version: 0.1.8 → 0.1.9

--- lazyqml/__init__.py
+++ lazyqml/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """QHPC Group (University of Oviedo)"""
 __email__ = "https://qhpc.uniovi.es"
-__version__ = "0.1.8"
+__version__ = "0.1.9"
 
 from .lazyqml import QuantumClassifier
 
